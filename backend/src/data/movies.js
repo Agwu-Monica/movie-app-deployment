93ad223cc8a1f523bc09ac34ecf38bf10bdbb866
Agwu-Monica <monichanny@gmail.com>
2026-05-07 19:21:45 +0100

    Initial AKS movie app deployment

---
+++ backend/src/data/movies.js
@@ -0,0 +1,7 @@
+const movies = [
+  { id: 1, title: "Avengers", year: 2012 },
+  { id: 2, title: "Batman Begins", year: 2005 },
+  { id: 3, title: "Spider-Man No Way Home", year: 2021 }
+];
+
+module.exports = movies;
